275cafafb756f86e7e15bf6a268d3e7fcd3e8ad0
fullstackjam <fullstackjam@outlook.com>
2026-02-06 21:27:48 +0800

    chore: bump version to v0.9.2

--- internal/installer/installer.go
+++ internal/installer/installer.go
@@ -27,7 +27,7 @@ func Run(cfg *config.Config) error {
 
 func runInstall(cfg *config.Config) error {
 	fmt.Println()
-	ui.Header("OpenBoot Installer v0.7.2")
+	ui.Header("OpenBoot Installer v0.9.2")
 	fmt.Println()
 
 	if cfg.DryRun {
